20f303739f02a2e21655e3a97e785ebe1881977d
Kabir Arora <152303949+KabirArora7@users.noreply.github.com>
2024-08-11 20:46:07 -0400

    Learning EJS

---
+++ EJS/4.0 EJS/index.js
@@ -0,0 +1,33 @@
+var express = require('express');
+const app = express();
+
+app.set('view engine', 'ejs');
+
+
+const d = new Date();
+let dayIdx = d.getDay();
+
+function checkDay(dayIdx) {
+    if (dayIdx == 0 || dayIdx == 6){
+        return "Weekend"
+    }
+    else {
+        return "Weekday"
+    }
+}
+
+const port = 3000
+
+
+app.get("/" , (req, res) =>{
+    const dayType = checkDay(dayIdx)
+    const data = {
+        dayType: dayType};
+    res.render("/Users/kabir/Desktop/Web Dev/EJS/4.0 EJS/views" , data)
+} )
+
+
+app.listen(port , ()=>{
+    console.log(`Server is listening on port ${port}`);
+
+});
